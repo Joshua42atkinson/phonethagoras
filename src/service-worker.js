7bd0cc91dad5f3e3ee32b7f90411fe4df90fd804
Joshua Atkinson <joshua42atkinson@gmail.com>
2026-06-05 06:27:36 -0400

    fix: bump SW to v3 with skipWaiting to force cache clear

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'phonethagoras-v2';
+const CACHE_NAME = 'phonethagoras-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -33,6 +33,7 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
@@ -49,7 +50,7 @@ self.addEventListener('activate', event => {
           }
         })
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
